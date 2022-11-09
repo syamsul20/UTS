2df59b6a7511cb4941b2d812bbb649050fffafa7
syamsul20 <114728043+syamsul20@users.noreply.github.com>
2022-11-09 21:13:40 +0700

    Add files via upload

---
+++ UTS_Jawaban2.py
@@ -0,0 +1,8 @@
+print("MENGHITUNG LUAS SEGITIGA")
+
+alas = int (input("Masukkan alas : "))
+tinggi = int (input("Masukkan tingi : "))
+
+luas = int (0.5*alas*tinggi)
+
+print("Luas Segitiga ",luas)
